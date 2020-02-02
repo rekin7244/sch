202ca835f343f63e4ca35b12d1a84e54a8d1a707
rekin7244 <rekint7244@gmail.com>
2020-02-02 11:53:36 +0900

    swagger 적응중..

--- src/main/java/com/kh/scheduler/controller/ToDoController.java
+++ src/main/java/com/kh/scheduler/controller/ToDoController.java
@@ -4,27 +4,54 @@ import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.kh.scheduler.common.GroupedApiResponses;
 import com.kh.scheduler.model.ToDo;
 import com.kh.scheduler.model.ToDoAdapter;
 import com.kh.scheduler.model.ToDoRequest;
 import com.kh.scheduler.model.ToDoResponse;
 import com.kh.scheduler.service.ToDoService;
 
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiImplicitParam;
+import io.swagger.annotations.ApiImplicitParams;
+import io.swagger.annotations.ApiOperation;
+
 @RestController
+@Api(tags = "todo", description = "할일 CRUD")
 public class ToDoController {
 
 	@Autowired
 	private ToDoService toDoService;
+
+	@ApiOperation(value = "할일 전체 조회" , notes = "할일 전체 조회")
+	@GetMapping(value = "/todo")
+	@GroupedApiResponses
+	public @ResponseBody List<ToDoResponse> selectToDoList() {
+		List<String> errors = new ArrayList<>();
+		List<ToDo> toDoList = toDoService.getAll();
+		List<ToDoResponse> toDoResponses = new ArrayList<>();
+		toDoList.stream().forEach(toDo -> {
+			toDoResponses.add(ToDoAdapter.toToDoResponse(toDo, errors));
+		});
+		return toDoResponses;
+	}
 	
-	@RequestMapping(method = RequestMethod.GET, value = "/todo/{id}")
-	public @ResponseBody ToDoResponse get(@PathVariable(value = "id") String id) {
+	@ApiOperation(value = "할일 조회" , notes = "할일 조회")
+	@ApiImplicitParams({
+        @ApiImplicitParam(name = "id", value = "할일 고유키", required = true, dataType = "string", paramType = "path", defaultValue = "")
+	})
+	@GroupedApiResponses
+	@GetMapping(value = "/todo/{id}")
+	public @ResponseBody ToDoResponse selectOneToDo(@PathVariable(value = "id") String id) {
 		List<String> errors = new ArrayList<>();
 		ToDo toDo = null;
 		
@@ -36,21 +63,13 @@ public class ToDoController {
 		return ToDoAdapter.toToDoResponse(toDo, errors);
 	}
 	
-	@RequestMapping(method = RequestMethod.GET, value = "/todo")
-	public @ResponseBody List<ToDoResponse> getAll() {
-		List<String> errors = new ArrayList<>();
-		List<ToDo> toDoList = toDoService.getAll();
-		List<ToDoResponse> toDoResponses = new ArrayList<>();
-		toDoList.stream().forEach(toDo -> {
-			toDoResponses.add(ToDoAdapter.toToDoResponse(toDo, errors));
-		});
-		return toDoResponses;
-	}
-	
-	@RequestMapping(method = RequestMethod.POST, value = "/todo")
-	public @ResponseBody ToDoResponse create(@RequestBody final ToDoRequest toDoRequest) {
+	@ApiOperation(value = "할일 추가" , notes = "할일 추가")
+	@GroupedApiResponses
+	@PostMapping(value = "/todo")
+	public @ResponseBody ToDoResponse insertToDo(@RequestBody final ToDoRequest toDoRequest) {
 		List<String> errors = new ArrayList<>();
 		ToDo toDo = ToDoAdapter.toToDo(toDoRequest);
+		
 		try {
 			toDo = toDoService.create(toDo);
 		} catch (final Exception e) {
@@ -58,4 +77,41 @@ public class ToDoController {
 		}
 		return ToDoAdapter.toToDoResponse(toDo, errors);
 	}
+	
+	@ApiOperation(value = "할일 수정" , notes = "할일 수정")
+	@ApiImplicitParams({
+		@ApiImplicitParam(name = "toDoRequest", value = "수정할 할일", required = true, dataType = "ToDo", paramType = "query", defaultValue = "")
+	})
+	@GroupedApiResponses
+	@PutMapping(value = "/todo")
+	public @ResponseBody ToDoResponse updateToDo(@RequestBody final ToDoRequest toDoRequest) {
+		List<String> errors = new ArrayList<>();
+		ToDo toDo = ToDoAdapter.toToDo(toDoRequest);
+		
+		try {
+			// update logic
+		} catch (final Exception e) {
+			errors.add(e.getMessage());
+		}
+		return ToDoAdapter.toToDoResponse(toDo, errors);
+	}
+	
+	@ApiOperation(value = "할일 삭제" , notes = "할일 삭제")
+	@ApiImplicitParams({
+		@ApiImplicitParam(name = "id", value = "할일 고유키", required = true, dataType = "string", paramType = "path", defaultValue = "")
+	})
+	@GroupedApiResponses
+	@DeleteMapping(value = "/todo")
+	public @ResponseBody ToDoResponse deleteToDo(@PathVariable(value = "id") String id) {
+		List<String> errors = new ArrayList<>();
+		ToDo toDo = null;
+		
+		try {
+			// delete logic
+		} catch (final Exception e) {
+			errors.add(e.getMessage());
+		}
+		return ToDoAdapter.toToDoResponse(toDo, errors);
+	}
+	
 }
